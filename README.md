221c24ca3d0810dea10968842976a5dfd2c7de2b
Harshit Cd <harshitcd@gmail.com>
2023-09-29 20:09:07 +0530

    Adds a method to build the script executable

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Terminal Wiki
+
+## Pre-Requisite
+``` terminal
+python3 -m pip install poetry
+```
+
+## Installation
+``` terminal
+poetry install
+poetry run pyinstaller --onefile terminal_wiki.py
+```
+
--- terminal_wiki.py
+++ terminal_wiki.py
@@ -1,12 +1,13 @@
 import requests
 import logging
+import sys
 from bs4 import BeautifulSoup
 import markdownify
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
-def main():
-    URL = "https://en.wikipedia.org/wiki/String_Theory"
+def scrape_wiki(text):
+    URL = f"https://en.wikipedia.org/wiki/{text}"
     r = requests.get(URL)
 
     soup  = BeautifulSoup(r.content, "html.parser")
@@ -17,7 +18,7 @@ def main():
 
     for tag in soup.find_all(tags):
 
-        if tag.text.strip().startswith("Notes"):
+        if tag.name != "p" and (tag.text.strip().startswith("Notes") or tag.text.strip().startswith("See also")):
             break
         if tag.text.strip() != "Contents":
             processed_s.append(f"<{tag.name}>{tag.text.strip()}</{tag.name}>")
@@ -26,9 +27,13 @@ def main():
     #     file.write("\n".join(processed_s))
 
     h = markdownify.markdownify("\n".join(processed_s), heading_style="ATX")
-    with open("data/output1.md", "w", encoding='utf-8') as file:
-        file.write(h)
+    # with open("data/output1.md", "w", encoding='utf-8') as file:
+    #     file.write(h)
+    
+    print(h)
 
+def main():
+    scrape_wiki("_".join(sys.argv[1:]).title())
 
 
 if __name__ == '__main__':
